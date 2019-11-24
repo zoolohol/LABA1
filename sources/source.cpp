d06278388dfd5c746c07fb0931192227b3605ebb
Anatoliy Sednev <stolya2000@mail.ru>
2019-11-24 20:01:22 +0300

    добавляю 2

---
+++ sources/source.cpp
@@ -0,0 +1,14 @@
+//Copyright 2019 <LinkIvan333>
+#include "Json.hpp"
+
+
+int main() {
+        string text;
+        text = R"({ "lastname" : "Ivanov" , "firstname" : "Ivan" , "age" : 25,"islegal" : false,"marks" : [ 4 , 5 , 5 , 5 , 2 , 3 ],"address" : {  "city" : "Moscow" ,  "street" : "Vozdvijenka" } })";
+        Json object = Json::parse(text);
+        cout << std::endl;
+        if (object.is_object())
+            cout << "Yes";
+
+    return 0;
+}
